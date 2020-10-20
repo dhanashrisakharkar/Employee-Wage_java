b6c5e93951cb0e8151c3c0a2fb69b67327727890
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-21 00:08:44 +0530

    Added : Employee Part Time Wage

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -6,15 +6,21 @@ public class Employee_Wage {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome To Employee Wage Computation In Java");
-		int Is_Full_Time = 1, Wage_Per_Hour = 20, Full_Day_Hour = 8;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		int Is_Full_Time = 1, Is_Part_Time=2,Wage_Per_Hour = 20, Full_Day_Hour = 8, Part_Time_Hour = 4;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 		System.out.println(empCheck);
 		if (empCheck == Is_Full_Time) {
 			System.out.println("Employee is present ");
 			int Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
-			System.out.printf("Daily Employee wage is :-",Daily_Wage);
+			System.out.printf("Daily Employee wage is :-", Daily_Wage);
 			System.out.println(Daily_Wage);
-		} else {
+		} else if (empCheck == Is_Part_Time) {
+			System.out.println("Employee is present ");
+			int Part_Time_Wage = Wage_Per_Hour * Part_Time_Hour;
+			System.out.printf("Daily Employee wage is :-",Part_Time_Wage );
+			System.out.println(Part_Time_Wage);
+		} 
+		else {
 			System.out.println("Employee is absent");
 		}
 
