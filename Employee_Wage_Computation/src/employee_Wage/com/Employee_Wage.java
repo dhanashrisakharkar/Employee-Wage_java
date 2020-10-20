3335193fde5f5050fa6014be28efd401f2978c1a
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-20 22:06:14 +0530

    Added : Employee Present Or Absent

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -5,6 +5,16 @@ public class Employee_Wage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("Welcome To Employee Wage Computation In Java");
+    int Is_Full_Time = 1;
+	int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+     System.out.println(empCheck);
+	if (empCheck == Is_Full_Time) {
+		System.out.println("Employee is present ");
+	} else {
+		System.out.println("Employee is absent");
+	}
+
+
 	}
 
 }
