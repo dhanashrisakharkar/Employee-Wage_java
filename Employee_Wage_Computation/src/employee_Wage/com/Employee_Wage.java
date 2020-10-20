a64d4648a073368111867b91549577f010327037
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-20 23:50:55 +0530

    Added : Daily Emplyee Wage

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -4,16 +4,19 @@ public class Employee_Wage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Welcome To Employee Wage Computation In Java");
-    int Is_Full_Time = 1;
-	int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-     System.out.println(empCheck);
-	if (empCheck == Is_Full_Time) {
-		System.out.println("Employee is present ");
-	} else {
-		System.out.println("Employee is absent");
-	}
 
+		System.out.println("Welcome To Employee Wage Computation In Java");
+		int Is_Full_Time = 1, Wage_Per_Hour = 20, Full_Day_Hour = 8;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		System.out.println(empCheck);
+		if (empCheck == Is_Full_Time) {
+			System.out.println("Employee is present ");
+			int Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
+			System.out.printf("Daily Employee wage is :-",Daily_Wage);
+			System.out.println(Daily_Wage);
+		} else {
+			System.out.println("Employee is absent");
+		}
 
 	}
 
