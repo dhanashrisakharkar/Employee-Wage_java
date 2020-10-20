0f12c9e3c1dd751e04bebd859fbe50636d63af98
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-20 22:03:45 +0530

    Added : Welcome Message

---
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -0,0 +1,10 @@
+package employee_Wage.com;
+
+public class Employee_Wage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Welcome To Employee Wage Computation In Java");
+	}
+
+}
