0f3162bf3d6efb01b732b8ee6a2102b85f54d856
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-22 10:04:03 +0530

    Added : Total Hours And Wages With Limits

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -7,12 +7,12 @@ public class Employee_Wage {
 
 		System.out.println("Welcome To Employee Wage Computation In Java");
 		int Wage_Per_Hour = 20;
-		int Full_Day_Hour = 8, Part_Time_Hour = 4;
 		int Total_Working_Day = 20;
 		int Total_Hours = 100;
 		int Days = 0;
 		int Hours = 1;
 		int Total_Wages = 0;
+		int Daily_Wage = 0;
 
 		while (Days <= Total_Working_Day && Hours <= Total_Hours) {
 
@@ -20,30 +20,21 @@ public class Employee_Wage {
 			System.out.println(empCheck);
 			switch (empCheck) {
 			case 1:
-				System.out.println("Employee is present ");
-				int Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
-				int FWages_Month = Daily_Wage * Total_Working_Day;
-				System.out.printf("Full Time employee Monthly Wage Is :-", FWages_Month);
-				System.out.println(FWages_Month);
-				Hours = Hours + Full_Day_Hour;
-				Total_Wages = Total_Wages + Daily_Wage;
-
+				System.out.println("Employee is present");
+				Hours = 8;
 				break;
 			case 2:
-				System.out.println("Employee is present ");
-				int Part_Time_Wage = Wage_Per_Hour * Part_Time_Hour;
-				int PWages_Month = Part_Time_Wage * Total_Working_Day;
-				System.out.printf("Par Time employee Daily Wage Is :-", PWages_Month);
-				System.out.println(PWages_Month);
-				Hours = Hours + Full_Day_Hour;
-				Total_Wages = Total_Wages + Part_Time_Wage;
+				System.out.println("Employee is Present");
+				Hours = 4;
 				break;
 			default:
 				System.out.println("Employee Is Absent");
 				break;
 			}
-
 			Days++;
+			Daily_Wage = Wage_Per_Hour * Hours;
+			Total_Wages = Total_Wages + Daily_Wage;
+			System.out.println("Employee Earn Daily Wage  :-" + Daily_Wage);
 			System.out.println("Employee Earn Total Wages is :-" + Total_Wages);
 		}
 	}
