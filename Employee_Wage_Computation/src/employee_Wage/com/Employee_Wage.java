f7ca3116664e7b8a8ce216e2b9609c8368ff44b5
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-23 14:17:06 +0530

    Added : More then one Company Details by User Input

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -1,12 +1,14 @@
 package employee_Wage.com;
 
+import java.util.Scanner;
+
 public class Employee_Wage {
 	public static int Hours = 0;
 	public static int Total_Wages = 0;
 	public static int Daily_Wage = 0;
 	public static int Days = 0;
 
-	public static int Company(String Company, int Total_Working_Day, int Total_Hours, int Wage_Per_Hour) {
+	public static void Company(String Company, int Total_Working_Day, int Total_Hours, int Wage_Per_Hour) {
 		int Days = 0;
 		while (Days <= Total_Working_Day && Hours <= Total_Hours) {
 
@@ -31,9 +33,9 @@ public class Employee_Wage {
 			System.out.println("Total Employee Days Is :-" + Days);
 			System.out.println("Employee Earn Daily Wage  :-" + Daily_Wage);
 			Total_Wages = Total_Wages + Daily_Wage;
+			System.out.println("Total Wage Per Employee :- " + Total_Wages);
 
 		}
-		return Total_Wages;
 
 	}
 
@@ -42,29 +44,28 @@ public class Employee_Wage {
 
 		System.out.println("***************Welcome To Employee Wage Computation*****************");
 
-		int CompanyCheck = (int) (Math.floor(Math.random() * 10) % 3 + 1);
-		System.out.println(CompanyCheck);
+		Scanner sc = new Scanner(System.in);
+		String companyName = null;
 
-		switch (CompanyCheck) {
-		case 1:
-			int Infosys = Company("Infosys", 20, 100, 30);
-			System.out.println("Employee Earn Total Wages in Infosys Company Is :-" + Infosys);
-			break;
-		case 2:
-			int TechMahindra = Company("TechMahindra", 25, 60, 40);
-			System.out.println("Employee Earn Total Wages in TechMahindra Company Is :-" + TechMahindra);
-			break;
-
-		case 3:
-			int Eton = Company("TechMahindra", 28, 80, 60);
-			System.out.println("Employee Earn Total Wages in Eton Company Is :-" + Eton);
-			break;
-
-		default:
-			System.out.println("No Company Is their");
-			break;
-
+		int exit = 0;
+		while (exit == 0) {
+			System.out.println("Enter your choice");
+			System.out.println("1.\t EXIT\t 2.\t calculate Employee Wage");
+			int choice1 = sc.nextInt();
+			if (choice1 == 1) {
+				exit = 1;
+			} else {
+				System.out.println("Enter company name");
+				companyName = sc.next();
+				System.out.println("Enter Working days in a month");
+				int Total_Working_Day = sc.nextInt();
+				System.out.println("Enter Working hours in a month");
+				int Total_Hours = sc.nextInt();
+				System.out.println("Enter emp wage per hour");
+				int Wage_Per_Hour = sc.nextInt();
+				Company(companyName, Total_Working_Day, Total_Hours, Wage_Per_Hour);
+			}
 		}
-
+		sc.close();
 	}
 }
