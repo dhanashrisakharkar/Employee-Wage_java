203ceb3c9ee201f78b8dd69d9ffffd29e08947dc
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-27 18:58:11 +0530

    Added : Add Daily And Monthly Wages

--- Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
+++ Employee_Wage_Computation/src/employee_Wage/com/Employee_Wage.java
@@ -10,13 +10,15 @@ public class Employee_Wage {
 	public static int Days = 0;
 	public static int Total_Array_Wage = 0;
 
-	public static int Company(String Company, int Total_Working_Day, int Total_Hours, int Wage_Per_Hour) {
+	public static int Company(String companyName, int Total_Working_Day, int Total_Hours, int Wage_Per_Hour) {
 		int Days = 0;
+		ArrayList list = new ArrayList();
+		list.add(companyName);
 		while (Days <= Total_Working_Day && Hours <= Total_Hours) {
 
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 			System.out.println(empCheck);
-
+            
 			switch (empCheck) {
 			case 1:
 				System.out.println("Employee is present");
@@ -34,10 +36,14 @@ public class Employee_Wage {
 			Daily_Wage = Wage_Per_Hour * Hours;
 			System.out.println("Total Employee Days Is :-" + Days);
 			System.out.println("Employee Earn Daily Wage  :-" + Daily_Wage);
+			list.add(Daily_Wage);
 			Total_Wages = Total_Wages + Daily_Wage;
 
 		}
+		list.add(Total_Wages);
+        System.out.println(list);
 		return Total_Wages;
+		
 
 	}
 
@@ -51,31 +57,33 @@ public class Employee_Wage {
 
 		int exit = 0;
 		while (exit == 0) {
+			int i=1;
 			System.out.println("Enter your choice");
 			System.out.println("1. calculate Employee Wage \n 2.exit");
 			int choice1 = sc.nextInt();
+			System.out.println("Enter the no of company");
+			int No = sc.nextInt();
+			while( i >= No)
+			{
 			if (choice1 == 1) {
-				ArrayList company = new ArrayList();
+		
 				System.out.println("Enter company name");
 				companyName = sc.next();
-				company.add(companyName);
 				System.out.println("Enter Working days in a month");
 				int Total_Working_Day = sc.nextInt();
-				company.add(Total_Working_Day);
 				System.out.println("Enter Working hours in a month");
 				int Total_Hours = sc.nextInt();
-				company.add(Total_Hours);
 				System.out.println("Enter emp wage per hour");
 				int Wage_Per_Hour = sc.nextInt();
-				company.add(Wage_Per_Hour);
 				System.out.println("Total Employee Wage Per Month Is :-" + Company(companyName, Wage_Per_Hour, Wage_Per_Hour, Wage_Per_Hour));
-                int Total_Array_Wage = Total_Wages;
-                company.add(Total_Array_Wage );
-				System.out.println(companyName + " Company Employee details IS :-" + company);
-
-			} else {
+			}else {
 				System.out.println("not acceptable");
 			}
+				
+			
+			}
+			
+			
 		}
 		sc.close();
 	}
